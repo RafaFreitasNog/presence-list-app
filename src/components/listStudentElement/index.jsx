def9584d35eec67ffa2973fead239a2d4d4df1c1
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-07-14 18:10:06 -0300

    added a grid to the list element tohelp organize the design, also added a 'delete' button who's functionality is to be implemented yet

--- src/components/listStudentElement/index.jsx
+++ src/components/listStudentElement/index.jsx
@@ -4,9 +4,14 @@ import './style.css'
 function StudentElement(props) {
     return (
         <Fragment>
-            <div id="listElement">
-                <h2 id="name">{props.name}</h2>
-                <h2 id="time">{props.time}</h2>
+            <div id="grid-conteiner">
+                <div id="grid-item1">
+                    <button id="delete-button">-</button>
+                </div>
+                <div id="grid-item2">
+                    <h2 id="name">{props.name}</h2>
+                    <h2 id="time">{props.time}</h2>
+                </div>
             </div>
         </Fragment>
     )
